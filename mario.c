3733063c051d058c265601341228d8e8bb84a9af
DFennec <DFennec@users.noreply.github.com>
2024-07-11 18:04:55 +0200

    confirmación automatizada por submit50

--- mario.c
+++ mario.c
@@ -3,26 +3,32 @@
 
 int main(void)
 {
-    int blockHeight=0;
-    do{
-        blockHeight=get_int("Height: \n");
-    }
-    while(blockHeight>8||blockHeight<1);
-
-    for(int i=0;i<blockHeight;i++)
+    int blockHeight = 0;
+    do
     {
-        for(int j=0;j<blockHeight-i;j++){
-            if(j==0){
+        blockHeight = get_int("Height: \n");
+    }
+    while (blockHeight > 8 || blockHeight < 1);
 
-            }else{
+    for (int i = 0; i < blockHeight; i++)
+    {
+        for (int j = 0; j < blockHeight - i; j++)
+        {
+            if (j == 0)
+            {
+            }
+            else
+            {
                 printf(" ");
             }
         }
-        for(int j=0;j<=i;j++){
+        for (int j = 0; j <= i; j++)
+        {
             printf("#");
         }
         printf("  ");
-        for(int j=0;j<=i;j++){
+        for (int j = 0; j <= i; j++)
+        {
             printf("#");
         }
         printf("\n");
