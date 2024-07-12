5de8d048003644e96b34f2d5c23ecf24e8080ab1
DFennec <DFennec@users.noreply.github.com>
2024-07-12 13:44:53 +0200

    confirmación automatizada por check50 [check50=True]

--- credit.c
+++ credit.c
@@ -1,37 +1,46 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <math.h>
-
+#include <stdio.h>
 
 string detectIssuer(long code);
 
 int main(void)
 {
-    long number=get_long("Number: ");
-    printf("\n%s",detectIssuer(number));
+    long number = get_long("Number: ");
+    printf("\n%s", detectIssuer(number));
 }
 
 bool valid(long code, int length)
 {
-    long firstSum=0;
-    long secondSum=0;
+    long firstSum = 0;
+    long secondSum = 0;
 
-    for(int i=1;i<length+1;i++){
-        long addedNum=((code%(long)(pow(10,i)))-(code%(long)(pow(10,i-1))))/(long)(pow(10,i-1));
-        if((i%2)==0){
-            addedNum*=2;
-            if(addedNum>9){
-                firstSum+=(addedNum-(addedNum%10))/10+(addedNum%10);
-            }else{
-                firstSum+=addedNum;
+    for (int i = 1; i < length + 1; i++)
+    {
+        long addedNum = ((code % (long) (pow(10, i))) - (code % (long) (pow(10, i - 1)))) /
+                        (long) (pow(10, i - 1));
+        if ((i % 2) == 0)
+        {
+            addedNum *= 2;
+            if (addedNum > 9)
+            {
+                firstSum += (addedNum - (addedNum % 10)) / 10 + (addedNum % 10);
             }
-        }else{
-            secondSum+=addedNum;
+            else
+            {
+                firstSum += addedNum;
+            }
+        }
+        else
+        {
+            secondSum += addedNum;
         }
     }
-    if((firstSum+secondSum)%10==0){
+    if ((firstSum + secondSum) % 10 == 0)
+    {
         return true;
-    }else
+    }
+    else
     {
         return false;
     }
@@ -39,29 +48,37 @@ bool valid(long code, int length)
 
 string detectIssuer(long code)
 {
-    int length=0;
-    while((code%(long)(pow(10,length)))!=code){
+    int length = 0;
+    while ((code % (long) (pow(10, length))) != code)
+    {
         length++;
     }
-    long start=((code%(long)(pow(10,length)))-(code%(long)(pow(10,length-2))))/(long)(pow(10,length-2));
-    long startFirst=(start-(start%10))/10;
+    long start = ((code % (long) (pow(10, length))) - (code % (long) (pow(10, length - 2)))) /
+                 (long) (pow(10, length - 2));
+    long startFirst = (start - (start % 10)) / 10;
 
-    if(valid(code,length))
+    if (valid(code, length))
     {
-    if((length==15)&&(start==34||start==37))
-    {
-        return "AMEX\n";
-    }else if((length==13||length==16)&&(startFirst==4)){
-        return "VISA\n";
+        if ((length == 15) && (start == 34 || start == 37))
+        {
+            return "AMEX\n";
+        }
+        else if ((length == 13 || length == 16) && (startFirst == 4))
+        {
+            return "VISA\n";
+        }
+        else if ((length == 16) &&
+                 (start == 51 || start == 52 || start == 53 || start == 54 || start == 55))
+        {
+            return "MASTERCARD\n";
+        }
+        else
+        {
+            return "INVALID\n";
+        }
     }
-    else if((length==16)&&(start==51||start==52||start==53||start==54||start==55)){
-        return "MASTERCARD\n";
-    }else{
+    else
+    {
         return "INVALID\n";
     }
-    }else
-    {
-        return "INVALID\n";
-    }
-
 }
