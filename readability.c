99fed60592c32d8ded7c06171adcab671ab6fd8d
DFennec <DFennec@users.noreply.github.com>
2024-07-15 22:38:10 +0200

    confirmación automatizada por submit50

--- readability.c
+++ readability.c
@@ -7,65 +7,76 @@
 double gradeText(string text);
 int main(void)
 {
-    string text= get_string("Text: ");
-    int grade=gradeText(text);
-    if(grade>16.0){
+    string text = get_string("Text: ");
+    int grade = gradeText(text);
+    if (grade > 16.0)
+    {
         printf("Grade 16+\n");
-    }else if(grade<1.0){
+    }
+    else if (grade < 1.0)
+    {
         printf("Before Grade 1\n");
-    }else{
-        printf("Grade %i\n",grade);
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
     }
 }
 
-double countLetters(string text)/*Average number of letters per 100 words*/
+double countLetters(string text) /*Average number of letters per 100 words*/
 {
-    double letters=0.0;
-    double totalLetters=0.0;
-    double words=0.0;
-    for (int i=0, n=strlen(text); i<n; i++)
+    double letters = 0.0;
+    double totalLetters = 0.0;
+    double words = 0.0;
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if(((ispunct(text[i])!=0)||(isblank(text[i])!=0)||(isspace(text[i])!=0))&&letters!=0)
+        if (((ispunct(text[i]) != 0) || (isblank(text[i]) != 0) || (isspace(text[i]) != 0)) &&
+            letters != 0)
         {
             words++;
-            totalLetters+=letters;
-            letters=0;
-        } else if(((ispunct(text[i])!=0)||(isblank(text[i])!=0)||(isspace(text[i])!=0))&&letters==0)
+            totalLetters += letters;
+            letters = 0;
+        }
+        else if (((ispunct(text[i]) != 0) || (isblank(text[i]) != 0) || (isspace(text[i]) != 0)) &&
+                 letters == 0)
         {
-            letters=0;
-        }else{
+            letters = 0;
+        }
+        else
+        {
             letters++;
         }
     }
-    return (totalLetters/words)*100;
+    return (totalLetters / words) * 100;
 }
 
-double countSentences(string text)/*Average sentences per 100 words*/
+double countSentences(string text) /*Average sentences per 100 words*/
 {
-    double sentences=0.0;
-    double words=0.0;
-    char prevChar='a';
-    char currChar='a';
-    for (int i=0, n=strlen(text); i<n; i++)
+    double sentences = 0.0;
+    double words = 0.0;
+    char prevChar = 'a';
+    char currChar = 'a';
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
-        currChar=text[i];
-        if(((ispunct(text[i])!=0)||(isblank(text[i])!=0)||(isspace(text[i])!=0))&&(isalpha(prevChar)!=0))
+        currChar = text[i];
+        if (((ispunct(text[i]) != 0) || (isblank(text[i]) != 0) || (isspace(text[i]) != 0)) &&
+            (isalpha(prevChar) != 0))
         {
             words++;
         }
-        if((ispunct(text[i])!=0)&&(currChar!=',')&&(currChar!='\''))
+        if ((ispunct(text[i]) != 0) && (currChar != ',') && (currChar != '\''))
         {
             sentences++;
         }
-        prevChar=text[i];
+        prevChar = text[i];
     }
-    return (sentences/words)*100;
+    return (sentences / words) * 100;
 }
 
 double gradeText(string text)
 {
-    double L=countLetters(text);
-    double S=countSentences(text);
-    double grade=((0.0588 * L) - (0.296 * S)) - 15.8;
+    double L = countLetters(text);
+    double S = countSentences(text);
+    double grade = ((0.0588 * L) - (0.296 * S)) - 15.8;
     return round(grade);
 }
