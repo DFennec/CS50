d6c742db0e72a49ae5f9ad35f063ca33c9ddfc44
DFennec <DFennec@users.noreply.github.com>
2024-07-18 11:57:54 +0200

    confirmación automatizada por submit50

--- plurality.c
+++ plurality.c
@@ -67,9 +67,9 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if(strcmp(candidates[i].name, name)==0)
+        if (strcmp(candidates[i].name, name) == 0)
         {
-            candidates[i].votes+=1;
+            candidates[i].votes += 1;
             return true;
         }
     }
@@ -79,19 +79,19 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    int maxVotes=0;
+    int maxVotes = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        if(maxVotes<candidates[i].votes)
+        if (maxVotes < candidates[i].votes)
         {
-            maxVotes=candidates[i].votes;
+            maxVotes = candidates[i].votes;
         }
     }
     for (int i = 0; i < candidate_count; i++)
     {
-        if(maxVotes==candidates[i].votes)
+        if (maxVotes == candidates[i].votes)
         {
-            printf("%s\n",candidates[i].name);
+            printf("%s\n", candidates[i].name);
         }
     }
     return;
