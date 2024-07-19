32e20a288c52e6da4ec555aeec044a096a201b68
DFennec <DFennec@users.noreply.github.com>
2024-07-19 14:36:02 +0200

    confirmación automatizada por check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -158,9 +158,11 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
+    int candidateVotes=0;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes > (voter_count / 2)+1)
+        candidateVotes=candidates[i].votes;
+        if ((float)candidateVotes > (float)(voter_count / 2))
         {
             printf("%s", candidates[i].name);
             return true;
