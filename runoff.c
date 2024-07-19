dc17fa8540e07b0898375dcf6c479dba27991691
DFennec <DFennec@users.noreply.github.com>
2024-07-19 15:06:37 +0200

    confirmación automatizada por submit50

--- runoff.c
+++ runoff.c
@@ -158,10 +158,10 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int candidateVotes=0;
+    int candidateVotes = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        candidateVotes=candidates[i].votes;
+        candidateVotes = candidates[i].votes;
         if (candidateVotes > (voter_count / 2))
         {
             printf("%s\n", candidates[i].name);
