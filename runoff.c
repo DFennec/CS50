ad1dc76333bd75a51443f95be9bbf6d4ec31584c
DFennec <DFennec@users.noreply.github.com>
2024-07-19 14:27:55 +0200

    confirmación automatizada por check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -141,6 +141,16 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
+        for (int i = 0; i < voter_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (candidates[preferences[i][j]].eliminated == false)
+            {
+                candidates[preferences[i][j]].votes =0;
+            }
+        }
+    }
     for (int i = 0; i < voter_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
@@ -160,7 +170,7 @@ bool print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if ((candidates[i].votes > (voter_count / 2)) && (candidates[i].eliminated == false))
+        if (candidates[i].votes > (voter_count / 2))
         {
             printf("%s", candidates[i].name);
             return true;
