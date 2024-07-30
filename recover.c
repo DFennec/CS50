10f88b29511e9d652f9132d86342d288975c4731
DFennec <DFennec@users.noreply.github.com>
2024-07-30 16:44:45 +0200

    confirmación automatizada por check50 [check50=True]

--- recover.c
+++ recover.c
@@ -10,55 +10,49 @@ int checkSignature(BYTE block[BLOCKSIZE]);
 
 int main(int argc, char *argv[])
 {
-    int serial = 0;
-    char stringifiedSerial[16]; // Larger buffer for serial number
+    int serial=0;
+    char stringifiedSerial[14];
 
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Usage: ./recover FILE\n");
         return 1;
     }
 
+    BYTE b;
+    FILE *card= fopen(argv[1], "r");
+    FILE *img;
     BYTE block[BLOCKSIZE];
-    FILE *card = fopen(argv[1], "r");
-    if (card == NULL) {
-        fprintf(stderr, "Error opening card file\n");
-        return 1;
-    }
+    while(fread(block, 1, BLOCKSIZE,card)==512)
+    {
 
-    FILE *img = NULL;
+        if(checkSignature(block)==0)
+        {
 
-    while (fread(block, 1, BLOCKSIZE, card) == BLOCKSIZE) {
-        if (checkSignature(block) == 0) {
-            if (img != NULL) {
+            if(serial>0)
+            {
                 fclose(img);
             }
 
-            sprintf(stringifiedSerial, "%03i.jpg", serial);
-            img = fopen(stringifiedSerial, "w");
-            if (img == NULL) {
-                fprintf(stderr, "Error creating image file\n");
-                return 1;
-            }
-            serial++;
-        }
+             sprintf(stringifiedSerial, "%03i.jpg", serial);
+             img=fopen(stringifiedSerial, "w");
+             serial++;
 
-        if (img != NULL) {
-            fwrite(block, 1, BLOCKSIZE, img);
         }
-    }
+        fwrite(block, 1, BLOCKSIZE, img);
 
-    if (img != NULL) {
-        fclose(img);
     }
     fclose(card);
+    if (img != NULL) {
+    fclose(img);
+    }
 
-    return 0;
 }
 
 int checkSignature(BYTE block[BLOCKSIZE])
 {
-    // Consider improving the signature check as needed
-    if (block[0] == block[2] && block[1] == 0xd8 && ((block[3] & 0xf0) == 0xe0)) {
+    if(block[0]==block[2]&&block[1]==0xd8&&((block[3] & 0xf0) == 0xe0))
+    {
         return 0;
     }
     return 1;
