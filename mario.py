205805b2b65f1a83f659aad6930e51a260de3344
DFennec <DFennec@users.noreply.github.com>
2024-08-14 17:23:03 +0200

    confirmación automatizada por check50 [check50=True]

--- mario.py
+++ mario.py
@@ -1,18 +1,17 @@
 n=0
 
-    while n<1 and n>8:
-        try:
-            n=int(input("Height: "))
-        except:
-            n=int(input("Height: "))
-        if n>0 and n<9:
-            for i in range(n):
-                for j in range(n-1-i):
-                    print(" ", end="")
-                for j in range(i+1):
-
-                    print("#", end="")
+while n<1 or n>8:
+    try:
+        n=int(input("Height: "))
+    except:
+        n=int(input("Height: "))
+    if n>0 and n<9:
+        for i in range(n):
+            for j in range(n-1-i):
                 print(" ", end="")
-                for j in range(i+1):
-                    print("#", end="")
-                print()
+            for j in range(i+1):
+                print("#", end="")
+            print("  ", end="")
+            for j in range(i+1):
+                print("#", end="")
+            print()
