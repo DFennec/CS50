0198a7c1a632ac8902cb23084b6f7ee7462da5b0
DFennec <DFennec@users.noreply.github.com>
2024-08-14 17:16:16 +0200

    confirmación automatizada por check50 [check50=True]

--- mario.py
+++ mario.py
@@ -1,14 +1,17 @@
-def mario(n):
-    for i in range(n):
-        for j in range(n-i):
-            print(" ", end="")
-        for j in range(i+1):
+n=0
+    while n<1 and n>8:
+        n=int(input("Height: "))
+        if n>0 and n<9:
+            for i in range(n):
+                for j in range(n-1-i):
+                    print(" ", end="")
+                for j in range(i+1):
 
-            print("#", end="")
-        print(" ", end="")
-        for j in range(i+1):
-            print("#", end="")
-        print()
+                    print("#", end="")
+                print(" ", end="")
+                for j in range(i+1):
+                    print("#", end="")
+                print()
+        else :
+            print("Invalid input.")
 
-
-mario(int(input("Height: ")))
