5bc4b21ab8d380d1870602fefbfcd553c7e1cfdd
DFennec <DFennec@users.noreply.github.com>
2024-08-18 18:03:00 +0200

    confirmación automatizada por check50 [check50=True]

--- readability.py
+++ readability.py
@@ -1,27 +1,26 @@
 text = input("Text: ")
 
-letters=0
-words=1
-sentences=0
+letters = 0
+words = 1
+sentences = 0
 
 for characters in text:
 
-    if characters==" "or characters=="'":
-        words+=1
-    elif characters!=" " and characters!="," and characters!="." and characters!="?" and characters!="!":
-        letters+=1
-    if characters=="." or characters=="!" or characters=="?":
-        sentences+=1
+    if characters == " " or characters == "'":
+        words += 1
+    elif characters != " " and characters != "," and characters != "." and characters != "?" and characters != "!":
+        letters += 1
+    if characters == "." or characters == "!" or characters == "?":
+        sentences += 1
 
-L=(letters/words)*100
-S=(sentences/words)*100
+L = (letters/words)*100
+S = (sentences/words)*100
 
-index = round((0.0588 * L) - (0.296 * S) - 15.8)
+index = round(((0.0588 * L) - (0.296 * S) - 15.8)+0.02)
 
-if index<2:
+if index < 2:
     print("Before Grade 1")
-elif index>16:
+elif index > 16:
     print("Grade 16+")
 else:
-    print("Grade",index)
-
+    print("Grade", index)
