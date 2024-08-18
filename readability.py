8f7cc7091ed6da8f3cdbec3b02efb635487ad1f7
DFennec <DFennec@users.noreply.github.com>
2024-08-18 17:58:01 +0200

    confirmación automatizada por check50 [check50=True]

---
+++ readability.py
@@ -0,0 +1,30 @@
+text = input("Text: ")
+
+letters=0
+words=1
+sentences=0
+
+for characters in text:
+
+    if characters==" "or characters=="'":
+        words+=1
+    elif characters!=" " and characters!="," and characters!="." and characters!="?" and characters!="!":
+        letters+=1
+    if characters=="." or characters=="!" or characters=="?":
+        sentences+=1
+
+print("L:",letters,"w:",words,"S:",sentences)
+L=(letters/words)*100
+S=(sentences/words)*100
+
+index = round((0.0588 * L) - (0.296 * S) - 15.8)
+
+print("idx:",index)
+
+if index<2:
+    print("Before Grade 1")
+elif index>16:
+    print("Grade 16+")
+else:
+    print("Grade",index)
+
